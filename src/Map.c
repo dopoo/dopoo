a9ac68b858a210cbf4ef9e82a37e13c995810109
dopoo <dopoo@abc.com>
2023-10-10 14:04:44 +0800

    update Map

--- src/Map.c
+++ src/Map.c
@@ -18,7 +18,7 @@ dopoo_mapD_applyRS(dopoo_mapD* map, dopoo_vec3D v)
 dopoo_vec3D
 dopoo_mapD_applyRST(dopoo_mapD* map, dopoo_vec3D v)
 {
-    return dopoo_vec3D_add(dopoo_mapD_applyRS(map, v), dopoo_vec3D_scale(map->t, map->s));
+    return dopoo_vec3D_add(dopoo_mapD_applyRS(map, v), map->t);
 }
 
 dopoo_vec3D
@@ -38,8 +38,8 @@ dopoo_vec3D
 dopoo_mapD_applyInvRST(dopoo_mapD* map, dopoo_vec3D v)
 {
     assert(map->s > deltaD);
-    return dopoo_vec3D_scale(dopoo_mapD_applyInvR(map, dopoo_vec3D_minus(v, 
-                             dopoo_vec3D_scale(map->t, map->s))), 1 / map->s);
+    return dopoo_vec3D_scale(dopoo_mapD_applyInvR(map, 
+                             dopoo_vec3D_minus(v, map->t)), 1 / map->s);
 }
 
 void
