fad0f71aabb42612bf5bf0fbf4f363a57738f066
dopoo <dopoo@abc.com>
2023-10-17 16:08:57 +0800

    update Render

--- src/Render.c
+++ src/Render.c
@@ -1,4 +1,6 @@
+#include <math.h>
 #include "../inc/Camera.h"
+#include "../inc/Math.h"
 
 
 void
@@ -53,8 +55,15 @@ dopoo_render_cylinder(const dopoo_camera* camera, double h, double r, dopoo_mapD
             {
                 dopoo_vec3D p0 = dopoo_rayD_computeP(&ray, t0);
                 double p0x = dopoo_vec3D_getx(p0);
+                double p0y = dopoo_vec3D_gety(p0);
                 double p0z = dopoo_vec3D_getz(p0);
-                dopoo_vec3D n = (dopoo_vec3D){p0x, 0, p0z};
+                dopoo_vec3D n;
+                if(fabs(p0x * p0x + p0z * p0z - r * r) < deltaD)
+                    n = (dopoo_vec3D){p0x, 0, p0z};
+                else if(p0y > h / 2)
+                    n = (dopoo_vec3D){0, 1, 0};
+                else
+                    n = (dopoo_vec3D){0, -1, 0};
                 n = dopoo_vec3D_norm(dopoo_mapD_applyRS(map, n));
                 double z = dopoo_vec3D_getz(n);
                 prgb = dopoo_vec3D_scale(rgb, z);
