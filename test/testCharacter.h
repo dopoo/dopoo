d3eede1525d961e1cf9275560e5a901cc2037f69
dopoo <dopoo@abc.com>
2023-11-01 15:01:01 +0800

    update test

--- test/testCharacter.h
+++ test/testCharacter.h
@@ -20,24 +20,30 @@ dopoo_testCreateCharacter()
     dopoo_vec3D t = {0, 0, 2};
     dopoo_camera* camera = dopoo_camera_create(CHRESOLUTION, CVRESOLUTION, q, t, ORTHOGRAPHICCAMERA);
 
-    dopoo_vec3D rgb0 = {1, 0, 0};
-    dopoo_vec3D rgb1 = {0, 1, 0};
-    dopoo_vec3D rgb2 = {0, 0, 1};
-    dopoo_vec3D rgb3 = {1, 1, 0};
-    dopoo_vec3D rgb4 = {1, 0, 1};
-    dopoo_vec3D rgb5 = {0, 1, 1};
-    dopoo_vec3D rgb6 = {0.5, 0, 0};
-    dopoo_vec3D rgb7 = {0, 0.5, 0};
-    dopoo_vec3D rgb8 = {0, 0, 0.5};
-    dopoo_vec3D rgb9 = {0.5, 0, 0.5};
-    dopoo_vec3D rgb10 = {0.5, 0.5, 0};
-    dopoo_vec3D rgb11 = {0, 0.5, 0.5};
-    dopoo_vec3D rgb12 = {1, 0.5, 0};
-    dopoo_vec3D rgb13 = {0.5, 1, 0};
-    dopoo_vec3D rgb14 = {0, 0.5, 1};
-    dopoo_vec3D rgb15 = {1, 0, 0.5};
-    dopoo_vec3D rgb16 = {0.5, 0.5, 1};
-    dopoo_vec3D rgb17 = {0.5, 1, 0.5};
+    dopoo_vec3D rgb0  = {0.867, 0.565, 0.455};
+    dopoo_vec3D rgb1  = {0.682, 0.494, 0.306};
+    dopoo_vec3D rgb2  = {0.651, 0.090, 0.106};
+    dopoo_vec3D rgb3  = {0.906, 0.780, 0.012};
+    dopoo_vec3D rgb4  = {0.039, 0.337, 0.529};
+    dopoo_vec3D rgb5  = {0.102, 0.427, 0.639};
+    dopoo_vec3D rgb6  = {0.843, 0.839, 0.769};
+    dopoo_vec3D rgb7  = {0.584, 0.302, 0.247};
+    dopoo_vec3D rgb8  = {1.000, 0.824, 0.05};
+    dopoo_vec3D rgb9  = {0.886, 0.812, 0.682};
+    dopoo_vec3D rgb10 = {0.784, 0.612, 0.310};
+    dopoo_vec3D rgb11 = {0.502, 0.569, 0.290};
+    dopoo_vec3D rgb12 = {0.970, 0.970, 0.839};
+    dopoo_vec3D rgb13 = {0.298, 0.263, 0.518};
+    dopoo_vec3D rgb14 = {0.871, 0.694, 0.235};
+    dopoo_vec3D rgb15 = {0.682, 0.682, 0.690};
+    dopoo_vec3D rgb16 = {0.647, 0.478, 0.106};
+    dopoo_vec3D rgb17 = {0.996, 0.970, 0.847};
+    dopoo_vec3D rgb18 = {1.000, 0.584, 0.251};
+    dopoo_vec3D rgb19 = {0.584, 0.510, 0.243};
+    dopoo_vec3D rgb20 = {0.910, 0.698, 0.322};
+    dopoo_vec3D rgb21 = {0.922, 0.545, 0.012};
+    dopoo_vec3D rgb22 = {0.000, 1.000, 0.000};
+    dopoo_vec3D rgb23 = {1.000, 1.000, 0.000};
 
 
     double waistSize = 0.1;
@@ -50,7 +56,7 @@ dopoo_testCreateCharacter()
     double w1 = 0.5;
     double d0 = 0.15;
     double d1 = 0.15;
-    dopoo_pyra* torso = dopoo_pyra_create(h, w0, w1, d0, d1, rgb0);
+    dopoo_pyra* torso = dopoo_pyra_create(h, w0, w1, d0, d1, rgb1);
 
     h = 0.4;
     double r0 = waistSize;
@@ -62,7 +68,7 @@ dopoo_testCreateCharacter()
 
     //abdomen
     r = 0.2;
-    dopoo_sphere* abdomen = dopoo_sphere_create(r, rgb16);
+    dopoo_sphere* abdomen = dopoo_sphere_create(r, rgb2);
 
     //waist
     double w = waistSize * 2;
@@ -80,17 +86,17 @@ dopoo_testCreateCharacter()
     w1 = 0.3;
     d0 = 0.15;
     d1 = 0.15;
-    dopoo_pyra* waist = dopoo_pyra_create(h, w0, w1, d0, d1, rgb2);
+    dopoo_pyra* waist = dopoo_pyra_create(h, w0, w1, d0, d1, rgb20);
 
     //left shoulder
     h = 0.35;
     r = 0.05;
-    dopoo_cylinder* lshoulder = dopoo_cylinder_create(h, r, rgb3);
+    dopoo_cylinder* lshoulder = dopoo_cylinder_create(h, r, rgb4);
 
     //right shoulder
     h = 0.35;
     r = 0.05;
-    dopoo_cylinder* rshoulder = dopoo_cylinder_create(h, r, rgb3);
+    dopoo_cylinder* rshoulder = dopoo_cylinder_create(h, r, rgb4);
 
     //left upper arm
     h = 0.3;
@@ -100,7 +106,7 @@ dopoo_testCreateCharacter()
 
     h = 0.3;
     r = 0.03;
-    dopoo_cylinder* lupperarm = dopoo_cylinder_create(h, r, rgb4);
+    dopoo_cylinder* lupperarm = dopoo_cylinder_create(h, r, rgb5);
 
     //right upper arm
     h = 0.3;
@@ -110,53 +116,53 @@ dopoo_testCreateCharacter()
 
     h = 0.3;
     r = 0.03;
-    dopoo_cylinder* rupperarm = dopoo_cylinder_create(h, r, rgb4);
+    dopoo_cylinder* rupperarm = dopoo_cylinder_create(h, r, rgb5);
 
     //left upper arm1
     h = 0.2;
     r = 0.08;
-    dopoo_cylinder* lupperarm1 = dopoo_cylinder_create(h, r, rgb9);
+    dopoo_cylinder* lupperarm1 = dopoo_cylinder_create(h, r, rgb6);
 
     //right upper arm1
     h = 0.2;
     r = 0.08;
-    dopoo_cylinder* rupperarm1 = dopoo_cylinder_create(h, r, rgb9);
+    dopoo_cylinder* rupperarm1 = dopoo_cylinder_create(h, r, rgb6);
 
     //left lower arm
     h = 0.3;
     r0 = 0.03;
     r1 = 0.05;
-    dopoo_cone* llowerarm = dopoo_cone_create(h, r0, r1, rgb6);
+    dopoo_cone* llowerarm = dopoo_cone_create(h, r0, r1, rgb7);
 
     //right lower arm
     h = 0.3;
     r0 = 0.03;
     r1 = 0.05;
-    dopoo_cone* rlowerarm = dopoo_cone_create(h, r0, r1, rgb6);
+    dopoo_cone* rlowerarm = dopoo_cone_create(h, r0, r1, rgb7);
 
     //left upper leg
     h = 0.4;
     r0 = 0.05;
     r1 = 0.08;
-    dopoo_cone* lupperleg = dopoo_cone_create(h, r0, r1, rgb7);
+    dopoo_cone* lupperleg = dopoo_cone_create(h, r0, r1, rgb8);
 
     //right upper leg
     h = 0.4;
     r0 = 0.05;
     r1 = 0.08;
-    dopoo_cone* rupperleg = dopoo_cone_create(h, r0, r1, rgb7);
+    dopoo_cone* rupperleg = dopoo_cone_create(h, r0, r1, rgb8);
 
     //left lower leg
     h = 0.3;
     r0 = 0.03;
     r1 = 0.05;
-    dopoo_cone* llowerleg = dopoo_cone_create(h, r0, r1, rgb8);
+    dopoo_cone* llowerleg = dopoo_cone_create(h, r0, r1, rgb9);
 
     //right lower leg
     h = 0.3;
     r0 = 0.03;
     r1 = 0.05;
-    dopoo_cone* rlowerleg = dopoo_cone_create(h, r0, r1, rgb8);
+    dopoo_cone* rlowerleg = dopoo_cone_create(h, r0, r1, rgb9);
 
     // left foot
     h = 0.1;
@@ -164,7 +170,7 @@ dopoo_testCreateCharacter()
     w1 = 0.1;
     d0 = 0.3;
     d1 = 0.1;
-    dopoo_pyra* lfoot = dopoo_pyra_create(h, w0, w1, d0, d1, rgb13);
+    dopoo_pyra* lfoot = dopoo_pyra_create(h, w0, w1, d0, d1, rgb10);
 
     // right foot
     h = 0.1;
@@ -172,42 +178,42 @@ dopoo_testCreateCharacter()
     w1 = 0.1;
     d0 = 0.3;
     d1 = 0.1;
-    dopoo_pyra* rfoot = dopoo_pyra_create(h, w0, w1, d0, d1, rgb13);
+    dopoo_pyra* rfoot = dopoo_pyra_create(h, w0, w1, d0, d1, rgb10);
 
 
     //neck
     h = 0.15;
     r = 0.05;
-    dopoo_cylinder* neck = dopoo_cylinder_create(h, r, rgb10);
+    dopoo_cylinder* neck = dopoo_cylinder_create(h, r, rgb11);
 
     //lower head
     h = 0.1;
     r0 = 0.05;
     r1 = 0.1;
-    dopoo_cone* lhead = dopoo_cone_create(h, r0, r1, rgb11);
+    dopoo_cone* lhead = dopoo_cone_create(h, r0, r1, rgb12);
 
     //middle head
     h = 0.15;
     r = 0.1;
-    dopoo_cylinder* mhead = dopoo_cylinder_create(h, r, rgb12);
+    dopoo_cylinder* mhead = dopoo_cylinder_create(h, r, rgb13);
 
     //left eye
     h = 0.05;
     r = 0.03;
-    dopoo_cylinder* leye = dopoo_cylinder_create(h, r, rgb14);
+    dopoo_cylinder* leye = dopoo_cylinder_create(h, r, rgb15);
 
     //left eye
     h = 0.05;
     r = 0.03;
-    dopoo_cylinder* reye = dopoo_cylinder_create(h, r, rgb14);
+    dopoo_cylinder* reye = dopoo_cylinder_create(h, r, rgb15);
 
     //left eye ball
     r = 0.02;
-    dopoo_sphere* leyeball = dopoo_sphere_create(r, rgb15);
+    dopoo_sphere* leyeball = dopoo_sphere_create(r, rgb22);
 
     //right eye ball
     r = 0.02;
-    dopoo_sphere* reyeball = dopoo_sphere_create(r, rgb15);
+    dopoo_sphere* reyeball = dopoo_sphere_create(r, rgb23);
 
     
     //joints
@@ -262,7 +268,7 @@ dopoo_testCreateCharacter()
     
 
     // create link
-    dopoo_link* link = dopoo_link_create(nodeCount, jointCount, rgb1);
+    dopoo_link* link = dopoo_link_create(nodeCount, jointCount, rgb16);
 
     //add node
     dopoo_link_addNode(link, node0);
@@ -305,7 +311,7 @@ dopoo_testCreateCharacter()
     dopoo_scene_addLink(scene, link);
 
     // transform
-    link->map.r = dopoo_quatD_create(0, -1, 0, 1);
+    link->map.r = dopoo_quatD_create(0, 1, 0, 1.5);
     torso->map.t = (dopoo_vec3D){0, 0.3, 0};
     abdomen->map.t = (dopoo_vec3D){0, 0.3, 0};
     waist->map.r = dopoo_quatD_create(1, 0, 0, 0);
@@ -314,13 +320,13 @@ dopoo_testCreateCharacter()
     mhead->map.t = (dopoo_vec3D){0, 0.825, 0};
 
     leye->map.r = dopoo_quatD_create(1, 0, 0, 1);
-    leye->map.t = (dopoo_vec3D){ 0.04, 0.85, 0.1};
+    leye->map.t = (dopoo_vec3D){ 0.04, 0.85, 0.08};
 
     reye->map.r = dopoo_quatD_create(1, 0, 0, 1);
-    reye->map.t = (dopoo_vec3D){-0.04, 0.85, 0.1};
+    reye->map.t = (dopoo_vec3D){-0.04, 0.85, 0.08};
 
-    leyeball->map.t = (dopoo_vec3D){ 0.04, 0.85, 0.115};
-    reyeball->map.t = (dopoo_vec3D){ -0.04, 0.85, 0.115};
+    leyeball->map.t = (dopoo_vec3D){ 0.04, 0.85, 0.095};
+    reyeball->map.t = (dopoo_vec3D){ -0.04, 0.85, 0.095};
 
     lshoulder->map.r = dopoo_quatD_create(0, 0, 1, 1);
     lshoulder->map.t = (dopoo_vec3D){0.23, 0.55, 0};
